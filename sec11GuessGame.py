7821da59da92a7381d0f90a6a0e95b48a3bada1e
sandipban <sandipban@gmail.com>
2021-08-04 17:50:30 +0530

    Update sec11GuessGame.py

--- sec11GuessGame.py
+++ sec11GuessGame.py
@@ -22,24 +22,3 @@ while True:
 print('Game over')
 
 
-'''Assignment Part 1 code'''
-
-punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
-
-def strip_punctuation(wrd):
-    '''This function takes a word as arguement and strip all punctuations in the list punctuation_chars'''
-    for pun in punctuation_chars:
-        wrd = wrd.replace(pun, '')
-    return wrd
-
-
-def get_pos(strn_sentence):
-    '''arguement strn is one or more sentences and calculate count of positive words'''
-    strn_sentence = strip_punctuation(strn_sentence)
-    lst_strn_sentence = strn_sentence.split()
-    cont = 0
-    for word in lst_strn_sentence:
-        for p_word in positive_words:
-            if word == p_word:
-                cont += 1
-    return cont
